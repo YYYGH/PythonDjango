734a1c500115517ce34288ec1f1c0b817277863d
YYYGH <yyyznt@163.com>
2017-04-11 09:38:20 +0800

    Update urls.py

--- mysite2/kpserver/urls.py
+++ mysite2/kpserver/urls.py
@@ -4,5 +4,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^index/$', views.index, name='index'),
-    url(r'^admin/', admin.site.urls),
+    #url(r'^admin/', admin.site.urls),
 ]
